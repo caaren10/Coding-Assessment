2d6402ada0aea82a7140b97ec25ef07651a08241
caaren10 <scmuyunga@g.ucla.edu>
2023-11-22 16:59:58 -0800

    add fixed JavaScript file

--- script.js
+++ script.js
@@ -1,95 +1,127 @@
+document.addEventListener("DOMContentLoaded", function() {
 var startElm = document.querySelector("#startButton");
-var questions = [
-    {
-        question: "Which HTML tag (< >) is used to link an external CSS file?"
-        choices: "style", "link","css", "script"
-    }
-    {
+var nextElm = document.querySelector("#next-button");
+var questionsCont = document.querySelector("#questions-container");
+var optionsCont = document.querySelector("#options-container");
+var resultsCont = document.querySelector("#results-container");
+var timer = document.querySelector("time");
 
+let currentQuestionIndex = 0;
+let score = 0;
+let timeRemaining = 60;
+
+
+questions = [
+    {
+        question: "Which HTML tag (< >) is used to link an external CSS file?",
+        choices: ["style", "link","css", "script"]
+        correctAnswer: "link",
+    },
+    {
+        question: "What does the typeof operator in JavaScript return for the data type of null?",
+        choices: ["undefined", "null", "object", "string"]
+        correctAnswer: "object",
     }
+    {
+        question: "In the context of frontend development, what does the term 'DOM' stand for?",
+        choices: ["Document Objext Model", "Data Object Model", "Document Oriented Model", "Design Object Model"]
+        correctAnswer: "Document Object Model",
+    }
+    {
+        question: "Which command is used to clone a Git repository from a remote URL?",
+        choices: ["git clone", "git pull", "git push," "git fetch"]
+        correctAnswer: "git clone",
+    }
+    {
+        question: "In a Node.js applications, what module is commonly used for handling asynchronous operations?",
+        choices: ["fs", "http", "async", "events"]
+        correctAnswer: "async"
+    }
+    {
+        question: "Which SQL clause is used to filter the results of a SELECT statement?",
+        choices: ["WHERE", "HAVING", "FILTER", "SELECT"]
+        correctAnswer: "WHERE",
+    }
+    {
+        question: "What does the CSS property 'margin:auto;' do?",
+        choices: ["Adds equal margins on all sides of an element", "Centers the element horizontally", "Centers the element vertically", "Removes all margina from the element"]
+        correctAnswer: "Centers the element horizontally",
+    }
+    {
+        question: "What is the purpose of the CSS 'z-index' property?",
+        choices: ["It sets the background color of an element", "It controls the order of positioning of stacked elements", "It adjusts the transparency of an element", "It defines the font size of an element"]
+        correctAnswer: "It controls the order of positioning of stacked elements",
+    }
+    {
+        question: "What is the difference between let, const, and var in JavaScript for declaring variables?",
+        choices: ["They are interchangeable and can be used in any context", "let is block-scoped, const is used for constants, and var is function-scoped", "var is block-scoped, let is function-scoped, and const is globally scoped", "const is block-scoped, let is globally scoped, and var is function-scoped"]
+        correctAnswer: "let is block-scoped, const is used for constants, and var is function-scoped",
+    }
+    {
+        question: "What is the difference between let, const, and var in JavaScript for declaring variables?",
+        choices: ["They are interchangeable and can be used in any context", "let is block-scoped, const is used for constants, and var is function-scoped", "var is block-scoped, let is function-scoped, and const is globally scoped", "const is block-scoped, let is globally scoped, and var is function-scoped"]
+        correctAnswer: "let is block-scoped, const is used for constants, and var is function-scoped",
+    }
+    {
+        question: "Explain the concept of event delegation JavaScript.",
+        choices: ["It refers to the process of handling events using inline event handlers", "It involves assigning multiple events to a single element.", "It is a technique where a single event listener is used to manage all the events for a group of child elements.", "It is the practice of delaying the execution of certain events."]
+        correctAnswer: "It is a technique where a single event listener is used to manage all the events for a group of child elements.",
+   }
 ]
 
-startElm.addEventListener('click', startQuiz)
-
-
-function displayQuestion(place){
-//     <!-- <div id="one">
-//     <h2> Which HTML tag (< >) is used to link an external CSS file?</h2>
-//     <h4><button class = "incorrectButton" class="A">A</button> ' style '</h4>
-//     <h4><button class = "correctButton" class="B">B</button> ' link ' </h4>
-//     <h4><button class = "incorrectButton" class="C">C</button> ' css '</h4>
-//     <h4><button class = "incorrectButton" class="D">D</button> ' script '</h4>   
-// </div>
-    var divElm = document.createElement('div');
-    var h2Elm = document.createElement('h2');
-    var buttonElm =document.createElement('button');
-
-
-    // add questions to elements
-    h2Elm.innerText(questions.question[place])
-
-}
-
 function startQuiz(){
-    // timer starts - doesnt need to be a dunction 
-    // start button gets deleted - not a function
-    // first question is showed
-    displayQuestion
+    startElm.style.display = "none";
+    nextElm.style.display = "block";
+    showQuestion(currentQuestionIndex);
+    startTimer();
 }
 
+function showQuestion(index){
+    var currentQuestion = questions[index];
+    questionsCont.text.Content = currentQuestions.question;
+    optionsCont.innerHTML="";
+    currentQuestion.options.forEach((option) => {
+        const button = document.createElement("button");
+        button.textContent = option;
+        button.addEventListener("click", () => {
+            checkAnswer(option, currentQuestion.correctAnswer);
+        });
+        optionsCont.appendChild(button);
+    });
+}
 
+function checkAnswer(selectOption, correctAnswer){
+    if (selectedOption === correctAnswer) {
+        score++;
+        resultsCont.textContent = "Correct!";
+    } else {
+        timeRemaining -= 10;
+        resultsCont.textContent = "Incorrect!";
+    }
+}
 
-// var ElstartButton = document.querySelector("#startButton");
-// var EltimeLeft =  document.querySelector(".timeLeft");
-// var ElallDone = document.querySelector(".allDone");
-// var ElhighScores = document.querySelector(".highScores");
-// var ElquestionContainer = document.querySelector(".question-container");
-// var ElScore = document.querySelector(".score");
-// var initialsInput =document.querySelector(".initials");
-// var ElHighScorespage = document.querySelector(".highScoresPage")
+currentQuestionIndex++;
+if (currentQuestionIndex < questions.length) {
+    showQuestion(currentQuestionIndex);
+} else {
+    endQuiz();
+}
 
-// let currentQuestionIndex = 0;
-// let EltimeLeft = 60;
-// let score = 0;
+function startTimer() {
+    const countdown = setInterval(function () {
+        if (timeRemaining <= 0 || currentQuestionIndex === questions.length) {
+            clearInterval(countdown);
+            endQuiz();
+        } else {
+            timer.textContent = timeRemaining;
+            timeRemaining--;
+        }
+    }, 1000);
+}
 
-// let timerInterval;
+function endQuiz() {
+    nextElm.style.display = "none";
+    resultsCont.textContent = `Game Over! Your score is ${score}.`;
+}
 
-// ElstartButton.addEventListener("click", startQuiz);
-
-// ElhighScores.addEventListener("click", showHighScores);
-
-// function startQuiz(event) {
-//     event.preventDefault();
-//     ElstartButton.computedStyleMap.display="none";
-//     displayQuestion();
-//     startTimer();
-// }
-
-// function displayQuestion(){
-
-
-// }
-
-// function startTimer(){
-//     timerInterval = setInterval(function (){
-//         if(EltimeLeft>0) {
-//             EltimeLeft --;
-//             EltimeLeftElement.textContent = EltimeLeft;
-//         }
-//         else {
-//             clearInterval(timerInterval);
-//             endQuiz();
-//         }
-//     }, 1000);
-// }
-
-// function endQuiz(){
-//     hideQuestions();
-//     questionContainers[questionContainers.length -1].style.display = "block";
-//     ElScore.textContent = score;
-//     initialsInput.style.display = "block";
-// }
-
-// function showHighScores(){
-//     ElHighScorespage.innerHTML = "High Scores Page";
-// }
+startElm.addEventListener('click', startQuiz);})
